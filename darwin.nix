203987404e07ee75fd02dad6a6f0575d2b04da49
Cooper Simpson <cooper@coopersimpson.com>
2026-03-14 14:54:24 +1000

    Disable Spotlight shortcut

--- darwin.nix
+++ darwin.nix
@@ -36,6 +36,21 @@
       NSAutomaticSpellingCorrectionEnabled = false;
       NSAutomaticCapitalizationEnabled = false;
     };
+
+    CustomUserPreferences = {
+      "com.apple.symbolichotkeys" = {
+        AppleSymbolicHotKeys = {
+          # Disable 'Cmd + Space' for Spotlight Search
+          "64" = {
+            enabled = false;
+          };
+          # Disable 'Cmd + Alt + Space' for Finder search window
+          "65" = {
+            enabled = false;
+          };
+        };
+      };
+    };
   };
 
   system.keyboard = {
